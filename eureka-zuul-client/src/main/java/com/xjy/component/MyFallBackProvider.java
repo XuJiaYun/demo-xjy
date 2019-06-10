e655857325bed9f6ea950754aa3bf6a036836632
xujiayun777 <xujiayun777@sina.com>
2019-06-10 17:27:04 +0800

    6.10

---
+++ eureka-zuul-client/src/main/java/com/xjy/component/MyFallBackProvider.java
@@ -0,0 +1,60 @@
+package com.xjy.component;
+
+import com.netflix.ribbon.proxy.annotation.Http;
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+//在Zuul中实现断路功能
+@Component
+public class MyFallBackProvider implements ZuulFallbackProvider {
+    @Override
+    public String getRoute() {
+        return "eureka-client";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream(("there is an error bro.".getBytes()));
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
